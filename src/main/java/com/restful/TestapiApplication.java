3e34773e92c0c4bac980f2e006007525d5a12005
ilham maulana <ims.ilham@gmail.com>
2022-08-11 10:01:25 +0700

    first-commit

---
+++ src/main/java/com/restful/TestapiApplication.java
@@ -0,0 +1,32 @@
+package com.restful;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restful.student.Student;
+
+@SpringBootApplication
+// membuat sebuah controler
+@RestController
+public class TestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestapiApplication.class, args);
+	}
+
+	// kode GET
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(new Student(1L,
+				"meriamBelina",
+				"Meriam@gmail.com",
+				LocalDate.of(2000, Month.JANUARY, 5), 21));
+	}
+
+}
